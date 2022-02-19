c22416cda03b33bf6be9825aeff0b8a353b4c3f3
Sphinx85 <91307559+Sphinx85@users.noreply.github.com>
2022-02-19 22:48:46 +0300

    PartOne Первая серия нашей учебы

---
+++ src/PartOne/MainClass.java
@@ -0,0 +1,33 @@
+package PartOne;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        byte b = 53;
+        short s = 4156;
+        int i = 441365;
+        long l = 415641L;
+
+        double d = 3.14;
+        float f = 456.4561F;
+
+        char c = 'A';
+        boolean bool = true;
+
+        System.out.println(b + " " + s + " " + i + " " + l + " " + d + " " + f + " " + c + " " + bool);
+
+        int x = 15616;
+        int y = 1456;
+        int z = 5;
+        int a = 776;
+
+        formula(x,y,z,a);
+    }
+
+    private static void formula(int x, int y, int z, int a) {
+        int result;
+        result = x * (y + (z / a));
+        System.out.println(result);
+    }
+}
